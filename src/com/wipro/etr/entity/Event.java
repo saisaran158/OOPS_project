8c0471615db39ccae431b1508a9815b921a1be9d
pradeepkumar2408 <pradeepkamaraj7639@gmail.com>
2026-05-26 14:12:50 +0530

    Event added

---
+++ src/com/wipro/etr/entity/Event.java
@@ -0,0 +1,50 @@
+package com.wipro.etr.entity;
+
+public class Event {
+
+	private String eventId;
+	private String eventName;
+	private String eventDate;
+	private int totalSeats;
+	private int availableSeats;
+	private double ticketPrice;
+	
+	public String getEventId() {
+		return eventId;
+	}
+	public void setEventId(String eventId) {
+		this.eventId = eventId;
+	}
+	public String getEventName() {
+		return eventName;
+	}
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+	public String getEventDate() {
+		return eventDate;
+	}
+	public void setEventDate(String eventDate) {
+		this.eventDate = eventDate;
+	}
+	public int getTotalSeats() {
+		return totalSeats;
+	}
+	public void setTotalSeats(int totalSeats) {
+		this.totalSeats = totalSeats;
+	}
+	public int getAvailableSeats() {
+		return availableSeats;
+	}
+	public void setAvailableSeats(int availableSeats) {
+		this.availableSeats = availableSeats;
+	}
+	public double getTicketPrice() {
+		return ticketPrice;
+	}
+	public void setTicketPrice(double ticketPrice) {
+		this.ticketPrice = ticketPrice;
+	}
+	
+	
+}
