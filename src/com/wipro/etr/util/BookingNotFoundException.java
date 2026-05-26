65dd1d58ab49b257ca34adb2bfa85d737c7dd27e
pradeepkumar2408 <pradeepkamaraj7639@gmail.com>
2026-05-26 14:18:14 +0530

    Pradeep Added

---
+++ src/com/wipro/etr/util/BookingNotFoundException.java
@@ -0,0 +1,11 @@
+package com.wipro.etr.util;
+
+public class BookingNotFoundException extends Exception{
+
+	@Override
+	public String toString() {
+		return "Booking Not Found []";
+	}
+	
+
+}
---
+++ src/com/wipro/etr/util/BookingOperationException.java
@@ -0,0 +1,10 @@
+package com.wipro.etr.util;
+
+public class BookingOperationException extends Exception{
+
+	@Override
+	public String toString() {
+		return "Booking Operation Error ";
+	}
+
+}
