17441177a8ac4e734d5bcefb906c540b6b8b27c8
Saisaran_p146 <saisaran158@gmail.com>
2026-05-26 14:20:08 +0530

    success

--- src/com/wipro/etr/util/EventNotFoundException.java
+++ src/com/wipro/etr/util/EventNotFoundException.java
@@ -1,5 +1,10 @@
 package com.wipro.etr.util;
 
 public class EventNotFoundException extends Exception{
+
+	@Override
+	public String toString() {
+		return "EventNotFoundException found here!";
+	}
 	
 }
